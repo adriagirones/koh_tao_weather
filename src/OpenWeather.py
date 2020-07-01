8ed88e10d66d1977027b1b24cf7ae76896453444
Adrià Gironès <girones94@gmail.com>
2020-07-01 17:46:35 +0700

    Modified the way how to declare variables on OpenWeather class

--- src/OpenWeather.py
+++ src/OpenWeather.py
@@ -1,3 +1,5 @@
+import datetime
+
 from src.Weather import Weather
 
 
@@ -5,18 +7,12 @@ class OpenWeather(Weather):
 
     def __init__(self, api: str):
         self.url = f'api.openweathermap.org/data/2.5/weather?'
-        self.city_name = ''
-        self.city_id = ''
-        self.lat = ''
-        self.lon = ''
-        self.country = ''
-        self.zip_code = ''
         self.url_city_name = self.url + f'q={self.city_name},{self.country}&appid={self.API}'
         self.url__city_id = self.url = f'id={self.city_id}&appid={self.API}'
         self.url_geographic_coordinates = self.url + f'lat={self.lat}&lon={self.lon}&appid={self.API}'
         self.url_zip_code = self.url + f'zip={self.zip_code},{self.country}&appid={self.API}'
-        self.humidity = ''
-        self.temperature = ''
-        self.pressure = ''
-        self.wind_information = ''
-        self.location = ''
+
+        self.city_name, self.country, self.location, self.country, self.wind_information = str
+        self.city_id, self.zip_code, self.visibility, self.humidity, self.pressure = int
+        self.sunrise, self.sunrise = datetime
+        self.lat, self.lon, self.temperature = float # TODO: convert far to cel
