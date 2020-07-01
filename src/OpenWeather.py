c6cdc6f26a7050bf8a107d4cbb283612ae6722be
Adrià Gironès <girones94@gmail.com>
2020-07-01 17:20:46 +0700

    Added variables on init for the params of the API

--- src/OpenWeather.py
+++ src/OpenWeather.py
@@ -15,3 +15,8 @@ class OpenWeather(Weather):
         self.url__city_id = self.url = f'id={self.city_id}&appid={self.API}'
         self.url_geographic_coordinates = self.url + f'lat={self.lat}&lon={self.lon}&appid={self.API}'
         self.url_zip_code = self.url + f'zip={self.zip_code},{self.country}&appid={self.API}'
+        self.humidity = ''
+        self.temperature = ''
+        self.pressure = ''
+        self.wind_information = ''
+        self.location = ''
