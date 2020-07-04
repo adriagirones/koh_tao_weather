4cfeb2891d9b0e98c47860f6e9d55f50633f65d7
Adrià Gironès <girones94@gmail.com>
2020-07-04 10:52:53 +0700

    Modified get_weather setting the values to the variables in the class OpenWeather

--- src/OpenWeather.py
+++ src/OpenWeather.py
@@ -1,21 +1,42 @@
 import datetime
 
+import requests
+
 from src.Weather import Weather
 
 
 class OpenWeather(Weather):
 
     def __init__(self, api: str):
-        self.url = f'api.openweathermap.org/data/2.5/weather?'
-        self.url_city_name = self.url + f'q={self.city_name},{self.country}&appid={self.API}'
-        self.url__city_id = self.url = f'id={self.city_id}&appid={self.API}'
-        self.url_geographic_coordinates = self.url + f'lat={self.lat}&lon={self.lon}&appid={self.API}'
-        self.url_zip_code = self.url + f'zip={self.zip_code},{self.country}&appid={self.API}'
-
-        self.city_name, self.country, self.location, self.country, self.wind_information = str
-        self.city_id, self.zip_code, self.visibility, self.humidity, self.pressure = int
-        self.sunrise, self.sunrise = datetime
-        self.lat, self.lon, self.temperature = float  # TODO: convert far to cel
+        self.api = api
+        self.lat = self.lon = self.wind = str
+        self.visibility = self.humidity = self.pressure = int
+        self.sunrise = self.sunrise = datetime
+        self.temperature = float  # TODO: convert far to cel
+        self.url = f'https://api.openweathermap.org/data/2.5/weather?'
 
     def get_weather(self, coordinates: tuple) -> str:
-        pass
+        """
+        Do API call to get the weather from a concrete coordinates
+        :param coordinates: tuple
+        :return: weather: str
+        """
+        self.lat = coordinates.__getitem__(0)
+        self.lon = coordinates.__getitem__(1)
+        url_geographic_coordinates = self.url + f'lat={self.lat}&lon={self.lon}&appid={self.api}'
+        response = requests.get(url_geographic_coordinates).json()
+        self.temperature = response['main']['temp']
+        self.visibility = response['visibility']
+        self.pressure = response['main']['pressure']
+        self.humidity = response['main']['humidity']
+        self.wind = f"Speed: {response['wind']['speed']}, degrees: response['wind']['deg']"
+        self.sunrise = response['sys']['sunrise']
+        self.sunrise = response['sys']['sunset']
+        weateher = f"{response['weather'][0]['main']}, {response['weather'][0]['description']}"
+
+        return weateher
+
+
+if __name__ == '__main__':
+    OP = OpenWeather('652661b4d9b718379cbe5cca2f4a0243')
+    info = OP.get_weather(('10.100051', '99.840210'))
