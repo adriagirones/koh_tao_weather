5717a210e1ce9fe028ee88f3e4f3510c8188a738
Adrià Gironès <girones94@gmail.com>
2020-07-04 18:49:38 +0700

    Added abstract methods on Weather class

--- src/Weather.py
+++ src/Weather.py
@@ -5,3 +5,19 @@ class Weather(ABC):
     def __init__(self):
         self.API = ''
         self.url = ''
+
+    @abstractmethod
+    def get_temperature(self):
+        pass
+
+    @abstractmethod
+    def get_wind(self):
+        pass
+
+    @abstractmethod
+    def get_visability(self):
+        pass
+
+    @abstractmethod
+    def get_weather(self):
+        pass
