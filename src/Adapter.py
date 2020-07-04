938281aac395b35cbceb2abfec6cb41f8bd1f2e1
stevenhiltrop <stevenhiltrop@gmail.com>
2020-07-04 19:36:09 +0700

    Refactored Adapter.py

--- src/Adapter.py
+++ src/Adapter.py
@@ -1,15 +1,16 @@
 from abc import ABC
 
+import numpy
+
 from OpenWeather import OpenWeather
 from Weather import Weather
-import numpy
 
 
 class Adapter(Weather, ABC):
 
     def __init__(self, coordinates: tuple):
         self._OP = OpenWeather('652661b4d9b718379cbe5cca2f4a0243', coordinates)
-        #self._CC = OpenWeather('', coordinates)
+        # self._CC = OpenWeather('', coordinates)
 
     def get_temperature(self):
         return numpy.mean(self._OP.temperature)
