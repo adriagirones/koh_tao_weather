f9e7cedb78efdae87ad0bab7d9af84286b3f7589
stevenhiltrop <stevenhiltrop@gmail.com>
2020-07-04 19:35:26 +0700

    Added ClimaCell.py self declaring variables

--- src/ClimaCell.py
+++ src/ClimaCell.py
@@ -4,10 +4,7 @@ from climacell_api.client import ClimacellApiClient
 class ClimaCell(ClimacellApiClient):
     def __init__(self, api: str, coordinates: tuple):
         super().__init__(api)
-        self.lat, self.lon = coordinates
-
-    def __repr__(self):
-        super().realtime(lat=self.lat, lon=self.lon, fields=[
+        response = super().realtime(lat=coordinates[0], lon=coordinates[1], fields=[
             'temp',
             'feels_like',
             'dewpoint',
@@ -32,6 +29,11 @@ class ClimaCell(ClimacellApiClient):
             'weather_code'
             # 'weather_groups'
         ])
+        data = {key: response.data().measurements[key].value for key, value in response.data().measurements.items()}
+        self.__dict__.update(data)
+
+    def __repr__(self):
+        pass
 
 
 if __name__ == '__main__':
