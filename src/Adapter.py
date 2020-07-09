cd809536b716c692cce181121dab01339802a10c
Adrià Gironès <girones94@gmail.com>
2020-07-09 12:56:48 +0700

    Added create_dataframe on Adapter clas

--- src/Adapter.py
+++ src/Adapter.py
@@ -4,21 +4,22 @@ import numpy
 from OpenWeather import OpenWeather
 from Weather import Weather
 from ClimaCell import ClimaCell
+import pandas as pd
+from itertools import chain
 
 
 def convert_degrees_compass_direction(degrees):
     wind_direction = {
-        "N": range(0, 23),
+        "N": chain(range(0, 23), range(338, 361)),
         "NE": range(23, 68),
         "E": range(68, 113),
         "SE": range(113, 158),
         "S": range(158, 203),
         "SW": range(203, 248),
         "W": range(248, 293),
-        "NW": range(293, 338),
-        "N": range(338, 361)
+        "NW": range(293, 338)
     }
-
+    value = ''
     for direction in wind_direction:
         if degrees in wind_direction[direction]:
             value = direction
@@ -33,6 +34,19 @@ class Adapter(Weather, ABC):
         # koh_tao coordinates = ('10.100051', '99.840210')
         self._OP = OpenWeather('652661b4d9b718379cbe5cca2f4a0243', coordinates)
         self._CC = ClimaCell('N9sVmSxG9QpcpJ7xidHgq9rkdSwjGDB1', coordinates)
+        self.create_dataframe()
+
+    def create_dataframe(self):
+        data = [
+            {'Temperature': self._OP.temperature, 'Wind Speed': self._OP.wind_speed,
+             'Wind Direction': f"{convert_degrees_compass_direction(round(self._OP.wind_degrees))}({self._OP.wind_degrees})",
+             'Weather': self._OP.weather_main},
+            {'Temperature': self._CC.temp, 'Wind Speed': self._CC.wind_speed,
+             'Wind Direction': f"{convert_degrees_compass_direction(round(self._CC.wind_direction))}({self._CC.wind_direction})",
+             'Weather': self._CC.weather_code}
+        ]
+        df = pd.DataFrame(data, columns=['Temperature', 'Wind Speed', 'Wind Direction', 'Weather'], index=['OW', 'CC'])
+        print(df)
 
     def get_temperature(self):
         return numpy.mean([self._OP.temperature, self._CC.temp])
@@ -44,3 +58,7 @@ class Adapter(Weather, ABC):
 
     def get_weather(self):
         pass
+
+
+if __name__ == '__main__':
+    adapter = Adapter(('10.100051', '99.840210'))
