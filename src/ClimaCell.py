db8f18b0cc94a321baf9a03229f35a6f365163ee
stevenhiltrop <stevenhiltrop@gmail.com>
2020-07-09 11:01:06 +0700

    Added ClimaCell.py documentation

--- src/ClimaCell.py
+++ src/ClimaCell.py
@@ -2,6 +2,25 @@ from climacell_api.client import ClimacellApiClient
 
 
 class ClimaCell(ClimacellApiClient):
+    """
+    Does a request to the API to get the current weather from a coordinates that you send.
+
+
+    :param: api: str, coordinates: tuple
+
+    :return: __repr__ : str
+    For example, we can get the info like this.
+    Temperature: 28.19
+    Humidity: 77.13
+    Wind:
+       Speed: 4.25
+       Gust: 4.5
+       Direction: 265.31
+    Sunrise: 2020-07-08T23:06:29.182Z
+    Sunset: 2020-07-09T11:47:24.965Z
+    Weather:
+       Main: cloudy
+    """
     def __init__(self, api: str, coordinates: tuple):
         super().__init__(api)
         response = super().realtime(lat=coordinates[0], lon=coordinates[1], fields=[
